ad33fccbb310ecdfac80436b87ca05d823223888
Micah Weech <mweechzing@gmail.com>
2017-11-03 13:43:13 -0500

    started new IOC project. Added new product database.

--- InversionOfControl/Controllers/HomeController.cs
+++ InversionOfControl/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using SqlIntro;
 
 namespace InversionOfControl.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [Dependency]
+        public IProductRepository ProductRepository { get; set; }
+    
+        
+        
+
+    public ActionResult Index()
         {
             return View();
         }
